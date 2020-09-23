5d9d96805811e4178a011948753130ddb5380397
ahd <ahdhani0@gmail.com>
2020-09-23 08:04:26 +0300

    signup error validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,30 @@
+const User = require("../models/User");
+
+// handle errors
+
+const handleErrors = (err) => {
+  let errors = {
+    email: "",
+    password: "",
+  };
+
+  // duplicate error code
+
+  if (err.code === 11000) {
+    errors.email = "email already registered";
+    return errors;
+  }
+  // validation errors
+
+  if (err.message.includes("user validation failed")) {
+    console.log(
+      Object.values(err.errors).forEach(({ properties }) => {
+        errors[properties.path] = properties.message;
+      })
+    );
+  }
+  return errors;
+};
 module.exports.signup_get = (req, res) => {
   res.send("sign up");
 };
@@ -6,13 +33,21 @@ module.exports.login_get = (req, res) => {
   res.send("login");
 };
 
-module.exports.signup_post = (req, res) => {
+module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
-  console.log(email, password);
-  res.send("new signup");
+  try {
+    const user = await User.create({
+      email,
+      password,
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    const errors = handleErrors(error);
+    res.status(400).json({ errors });
+  }
 };
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
   console.log(email, password);
   res.send("user login");
